f13e3d664185aa55cfe6543559d93716085a868f
programmer4229 <ggonzalez77@gatech.edu>
2024-03-01 18:20:38 -0500

    Initial Commit

---
+++ Paperless/PaperlessApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaperlessApp.swift
+//  Paperless
+//
+//  Created by Gaby Gonzalez on 3/1/24.
+//
+
+import SwiftUI
+
+@main
+struct PaperlessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
